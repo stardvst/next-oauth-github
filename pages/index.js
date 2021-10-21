a35143058a542a0c3fb1919f8e1baeec541965de
Lilia <lilia.manukyan@hotmail.com>
2021-10-21 22:03:22 +0400

    saving data to database for signed in user

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,43 @@
 import { getSession, signIn, signOut } from 'next-auth/react';
+import { useState } from 'react';
 
 export default function Component({ session }) {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
   if (session) {
     return (
       <>
         Signed in as {session.user.email} <br />
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            fetch('/api/posts', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ title, body }),
+            });
+          }}
+        >
+          <div>
+            <label htmlFor="title">Title:</label>
+            <input
+              onChange={e => setTitle(e.target.value)}
+              type="text"
+              name="title"
+            />
+          </div>
+          <div>
+            <label htmlFor="body">Body:</label>
+            <textarea
+              onChange={e => setBody(e.target.value)}
+              name="body"
+            ></textarea>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
         <button onClick={() => signOut()}>Sign out</button>
       </>
     );
