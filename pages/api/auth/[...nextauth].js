0703725d66bb13e2c257ba1b512ab76af682eadf
Lilia <lilia.manukyan@hotmail.com>
2021-10-27 22:11:52 +0400

    add few next auth options

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -16,5 +16,29 @@ export default NextAuth({
     signingKey: process.env.JWT_SIGNING_PRIVATE_KEY,
   },
   adapter: PrismaAdapter(prisma),
+  session: {
+    jwt: false,
+    maxAge: 30 * 60 * 60 * 24, // 30 days
+    updateAge: 10 * 60 * 60 * 24, // every 10 days, extend session by 30 days
+  },
   database: process.env.DATABASE_URL,
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      console.log('signIn: ', user, account, profile, email, credentials);
+      return true;
+    },
+    async redirect({ url, baseUrl }) {
+      console.log('redirect: ', url, baseUrl);
+      return baseUrl;
+    },
+    async session({ session, user, token }) {
+      console.log('session: ', session, user, token);
+      session.token = token;
+      return session;
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      console.log('jwt: ', token, user, account, profile, isNewUser);
+      return token;
+    },
+  },
 });
